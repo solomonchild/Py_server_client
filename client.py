4e4aa5c8c74e0738fe50df2b47facf3c92d3a65c
vlad <solomonchild@gmail.com>
2013-07-05 15:39:22 +0300

    Added nicknames

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import socket
 def main():
 
   while(1):
+    name = raw_input("your nickname: ")
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server,port = raw_input("server:port : ").split(":")
     if((server is '') | (not port.isdigit())):
@@ -11,13 +12,14 @@ def main():
 	continue
     try:
       sock.connect((server,int(port)))
+      sock.send("NICKNAME:"+name)
     except socket.error, err:
       print ("Could not connect to {}:{}\n{} ".format(server,port,err))
       continue
 
     while(1):
       data = raw_input("enter message: ")
-      sock.send(data)
+      sock.send("DATA:"+data)
       if(data == "/dc"):
 	print "disconnected from ", server
 	sock.close()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 import socket
 
+users = {}
 def main():
   sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
   sock.bind(("localhost",2013))
@@ -10,12 +11,19 @@ def main():
     print "user [",addr,"] has connected"
     while 1:
       data = conn.recv(1024)
-      if((data!=None) & (len(data)!=0) & (data!="/dc")):
-	print data
-      else:
-	print "user [",addr,"] has disconnected"
-	conn.close()
-	break
+      if((data!=None) & (len(data)!=0)):
+	tag,value = data.split(":")
+	if(tag == "NICKNAME"):
+	  if addr not in users:
+	   users[addr] = value
+	elif tag=="DATA":
+	  if(value != "/dc"):
+	    if(addr in users):
+	      print users[addr],": ",value 
+	  else:
+	    print "user [",addr,"] has disconnected"
+	    conn.close()
+	    break
 
 if __name__=="__main__":
   main()
